def685f78469962ca5d709c8c3d9c27dc6cfa069
marikanaumova <marusencia.arz.glam@mail.ru>
2022-02-05 23:19:59 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const img = document.getElementById('img-btn')
+
+const randomInteger = (min, max) => {
+    let rand = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(rand)
+} 
+
+const soundsMap = {
+    1: '1.mp3',
+    2: '2.mp3',
+    3: '3.mp3',
+}
+
+const randomAudio = () => {
+    const sound = new Audio()
+    if (randomInteger(1, 3) === 1) {
+        sound.src = soundsMap[1]
+        sound.play()
+    } else if (randomInteger(1, 3) === 2) {
+        sound.src = soundsMap[2]
+        sound.play()
+    } else {
+        sound.src = soundsMap[3]
+        sound.play()
+    }
+}
+
+img.onclick = () => {
+    randomAudio()
+}
